93b8e680934e26deb122f09e9c806555b7ee9634
Enfei Liao <l.enfei@outlook.com>
2018-01-26 05:35:59 +0800

    Update RegexParser.java
    Update URL regex

--- autolinklibrary/src/main/java/com/luseen/autolinklibrary/RegexParser.java
+++ autolinklibrary/src/main/java/com/luseen/autolinklibrary/RegexParser.java
@@ -12,7 +12,5 @@ class RegexParser {
     static final String EMAIL_PATTERN = Patterns.EMAIL_ADDRESS.pattern();
     static final String HASHTAG_PATTERN = "(?:^|\\s|$)#[\\p{L}0-9_]*";
     static final String MENTION_PATTERN = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*";
-    static final String URL_PATTERN = "(^|[\\s.:;?\\-\\]<\\(])" +
-            "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
-            "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])";
+    static final String URL_PATTERN = "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])";
 }
